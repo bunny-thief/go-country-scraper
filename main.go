3a38d1470ac195a24862bb27d46e5e8b7ebd54be
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-02-06 13:49:21 +0100

    Scrape country data

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+	"strconv"
+)
+
+type country struct {
+	Country    string
+	Capital    string
+	Population int
+	Area float32
+}
+
+func main() {
+	countries := []country{	}
+
+	c := colly.NewCollector(
+		colly.MaxDepth(1),
+	)
+
+	c.OnHTML(".country", func(h *colly.HTMLElement) {
+		country := country{}
+		country.Country = h.ChildText(".country-name")
+		fmt.Println(country.Country)
+		country.Capital = h.ChildText(".country-capital")
+		fmt.Println(country.Capital)
+		population , err := strconv.Atoi(h.ChildText(".country-population"))
+
+		if err != nil {
+			panic(err)
+		}
+
+		country.Population = population
+		fmt.Println(country.Population)
+
+		area, err := strconv.ParseFloat(h.ChildText(".country-area"), 32)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Calling ParseFloat with a bitSize of 32 returns a float64
+		// that needs to be converted to float32
+		country.Area = float32(area)
+		fmt.Println(country.Area)
+
+		countries = append(countries, country)
+		fmt.Println("")
+	})
+
+	c.Visit("https://www.scrapethissite.com/pages/simple/")
+
+}
